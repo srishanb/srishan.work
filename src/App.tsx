5a9d638d6ca9d2e4baed7dc53ce5e8021408cbb8
srishanb <bsrishan@gmail.com>
2026-03-02 17:53:00 -0500

    created landing page for website while working. Planning to just host this so I have a working website

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
-import ListGroup from "./components/ListGroup";
-
 function App() {
-    return <div><ListGroup>
+    return (
+        <div className="d-flex flex-column justify-content-center align-items-center min-vh-100 text-center">
+            <h1 className="mb-4">Website currently under construction!</h1>
 
-    </ListGroup></div>;
+            <div className="d-grid col-6 col-md-4 mx-auto">
+                <a
+                    href="https://github.com/srishanb/srishan.work"
+                    className="btn btn-primary"
+                    target="_blank"
+                    rel="noopener noreferrer"
+                >
+                    The progress
+                </a>
+            </div>
+        </div>
+    );
 }
 
 export default App;
